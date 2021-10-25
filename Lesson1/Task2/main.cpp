bdefabaedf4a314d12fe35cbe954ee833cb71820
Олександр Гончаренко <s.goncharenlo.09.08@gmail.com>
2021-10-25 17:00:34 +0300

    added tsk 2 to lesson 1

---
+++ Lesson1/Task2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "       _________" << endl;
+    cout << "      (=========)" << endl;
+    cout << "      |=========|" << endl;
+    cout << "      |====_====|" << endl;
+    cout << "      |== / \\ ==|" << endl;
+    cout << "      |= / _ \\ =|" << endl;
+    cout << "   _  |=| ( ) |=|" << endl;
+    cout << "  /=\\ |=|     |=| /=\\" << endl;
+    cout << "  |=| |=| USA |=| |=|" << endl;
+    cout << "  |=| |=|  _  |=| |=|" << endl;
+    cout << "  |=| |=| | | |=| |=|" << endl;
+    cout << "  |=| |=| | | |=| |=|" << endl;
+    cout << "  |=| |=| | | |=| |=|" << endl;
+    cout << "  |=| |/  | |  \\| |=|" << endl;
+    cout << "  |=|/    | |    \\|=|" << endl;
+    cout << "  |=/NASA |_| NASA\\=|" << endl;
+    cout << "  |(_______________)|" << endl;
+    cout << "  |=| |_|__|__|_| |=|" << endl;
+    cout << "  |=|   ( ) ( )   |=|" << endl;
+    cout << " /===\\           /===\\" << endl;
+    cout << "|||||||         |||||||" << endl;
+    cout << "-------         -------" << endl;
+    cout << " (~~~)           (~~~)" << endl;
+
+    return 0;
+}
