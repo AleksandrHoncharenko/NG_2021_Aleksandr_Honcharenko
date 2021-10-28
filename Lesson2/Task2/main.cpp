f4786e80a563b9273e2279d230852d2f3f198eb1
Олександр Гончаренко <s.goncharenlo.09.08@gmail.com>
2021-10-28 17:49:04 +0300

    added task 2 to lesson 2

---
+++ Lesson2/Task2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+ int Hash, Number, LastDigit = -1, FirstDigit, SecondDigit;
+ cout << "Enter ur number: ";
+ cin >> Number;
+
+ // assigning a value to first and second digit
+ for(int x = Number; x >= 1; x /=10){
+    FirstDigit = x % 10;
+    SecondDigit = x / 10 % 10;
+
+    // save last digit
+    if (LastDigit == -1)
+        LastDigit = FirstDigit;
+    // check the same digit
+    if(FirstDigit == SecondDigit)
+        Hash += FirstDigit;
+ }
+ // identity check first and last digit
+ if (FirstDigit == LastDigit){
+    Hash += FirstDigit;
+ }
+ cout << "Hash of ur number = " << Hash;
+    return 0;
+}
